bfed7ec312bdaef7d80644015bd3ff84473647c3
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-04-03 07:53:31 -0300

    findById Implementation

--- src/application/model/dao/DaoFactory.java
+++ src/application/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package application.model.dao;
 
 import application.model.dao.impl.SellerDaoJDBC;
+import db.DB;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
