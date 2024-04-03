efca2dd659a32ce1cc21deb020d7f0d9714be581
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-04-03 09:04:36 -0300

    insert Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import application.model.dao.SellerDao;
 import application.model.entities.Department;
 import application.model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -30,5 +31,10 @@ public class Program {
         for(Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n==== TEST 4: seller insert ====");
+        Seller newSeller =  new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id: " +  newSeller.getId());
     }
 }
